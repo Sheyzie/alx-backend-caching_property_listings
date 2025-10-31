80fc5a34f53ba75c692085640630174524a1408d
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-10-31 18:47:21 +0100

    Cache Metrics Analysis

--- properties/utils.py
+++ properties/utils.py
@@ -1,4 +1,5 @@
 from django.core.cache import cache
+from django_redis import get_redis_connection
 
 from .models import Property
 
@@ -17,3 +18,21 @@ def get_all_properties():
     cache.set('all_properties', queryset, 3600)
 
     return queryset
+
+def get_redis_cache_metrics():
+    # Get a raw redis-py client
+    redis_client = get_redis_connection("default")
+
+    info = redis_client.info()  # same as redis-cli INFO
+
+    hits = info.get("keyspace_hits", 0)
+    misses = info.get("keyspace_misses", 0)
+
+    total = hits + misses
+    hit_ratio = (hits / total) if total > 0 else 0
+
+    return {
+        "hits": hits,
+        "misses": misses,
+        "hit_ratio": round(hit_ratio, 3),
+    }
