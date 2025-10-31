24f2b62eb0bbe1eeed698d53a3ed9d4efe19d1ff
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-10-31 18:11:58 +0100

    Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+from .models import Property
+from .serializers import PropertySerializer
+
+@api_view(['GET'])
+@cache_page(60 * 15)  # cache for 15 minutes
+def property_list(request):
+    properties = Property.objects.all()
+    serializer = PropertySerializer(properties, many=True)
+    data = serializer.data
+
+    return Response(data, status=status.HTTP_200_OK)
+
+# @api_view(['GET'])
+# def property_list(request):
+#      # Check cache first
+#     cache_key = "property_list"
+#     data = cache.get(cache_key)
+
+#     if data is not None:
+#         return Response(data, status=status.HTTP_200_OK)
+
+#     # Otherwise, fetch from DB
+#     properties = Property.objects.all()
+#     serializer = PropertySerializer(properties, many=True)
+#     data = serializer.data
+
+#     # Store in cache for 10 minutes (600 seconds)
+#     cache.set(cache_key, data, timeout=600)
+
+#     return Response(data)
+
